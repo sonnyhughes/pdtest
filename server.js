4d8890c415ee72aa93b7836aed2a707935875afb
Sonny <richardhughesjr@gmail.com>
2018-03-26 19:22:44 -0500

    Adds comments to server.js

--- server.js
+++ server.js
@@ -11,83 +11,64 @@ var partials = require('express-partials');
 var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
-//for github passport
+//GITHUB PASSPORT
 var GITHUB_CLIENT_ID = "caff6bf6f0c8379836f4";
 var GITHUB_CLIENT_SECRET = "34e26a2b6b29e0e58ccc1ea8e60ad3f119baa5c0";
 
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
-//   and deserialized.
+// PASSPORT SESSION SETUP
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
-
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
 
-// Use the GitHubStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
-//   profile), and invoke a callback with a user object.
+// USE GITHUB STRATEGY WITHIN PASSPORT
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
     callbackURL: "https://projectdepottest.herokuapp.com/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-
     process.nextTick(function () {
-      
       return done(null, profile);
     });
   }
 ));
 
-
-
-//configuring express app
+// EXPRESS APP
 var app = express();
 app.use(partials());
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 app.use(methodOverride("_method"));
 
-// Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static(process.cwd() + "/public"));
-
-//using bodyparser for post and put data
+//BODY PARSER FOR POST AND PUT
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Initialize Passport!  Also use passport.session() middleware, to support
-// persistent login sessions (recommended).
+// INIT PASSPORT
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(express.static(__dirname + '/public'));
 
-// Links the static content (i.e. css and images)
+// STATIC CONTENT
 app.use(express.static(__dirname + '/public'));
 // 
 
-// Set the engine up for handlebars
+// HANDLEBARS ENGINE
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-//importing routes
+//IMPORT ROUTES
 var sandboxRoutes = require("./controllers/sandbox_controller.js")(app);
 
 var loginRoutes = require("./controllers/login_controller.js")(app);
 
 var pageRoutes = require("./controllers/pages_controller.js")(app);
 
-//syncing database and listening 
+//DATABASE LISTENING
 db.sequelize.sync().then(function() {
     app.listen(PORT, function () {
 console.log("App listening this awesome PORT: " + PORT);
